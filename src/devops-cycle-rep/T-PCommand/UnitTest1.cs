40c7faa82d5179533271f514071bbef06c37b75b
hossein193 <hosseingolmohammadi193201@gmail.com>
2023-07-17 22:45:01 +0430

    update package and add test project

---
+++ src/devops-cycle-rep/T-PCommand/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace T_PCommand
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/devops-cycle-rep/T-PQuery/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace T_PQuery
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
